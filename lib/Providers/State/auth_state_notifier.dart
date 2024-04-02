7d9606ff4ef87b70f615bc879c1e28ef93df9406
Liman <saniahmadliman@gmail.com>
2024-04-02 12:59:19 -0800

    make some changes

--- lib/Providers/State/auth_state_notifier.dart
+++ lib/Providers/State/auth_state_notifier.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:instantgram/Constants/Posts/user_id.dart';
 import 'package:instantgram/Models/auth_result.dart';
