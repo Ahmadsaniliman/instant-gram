6616ac0efc44b91a08e39fb2445c4e2af24f9b8d
Liman <saniahmadliman@gmail.com>
2024-04-08 13:00:34 -0800

    make some changes

--- lib/Widgets/Loading/loading_screen.dart
+++ lib/Widgets/Loading/loading_screen.dart
@@ -18,7 +18,7 @@ class LoadingScreen {
     if (_controller?.update(text) ?? false) {
       return;
     } else {
-      _controller = showOverlay(
+      _controller = showOverLay(
         context: context,
         text: text,
       );
@@ -26,80 +26,78 @@ class LoadingScreen {
   }
 
   void hide() {
-    _controller?.close;
+    _controller?.close();
     _controller == null;
   }
 
-  LoadingScreenController? showOverlay({
+  LoadingScreenController? showOverLay({
     required BuildContext context,
     required String text,
   }) {
+    final textController = StreamController<String>();
+    textController.add(text);
+
     final state = Overlay.of(context);
     // ignore: unnecessary_null_comparison
     if (state == null) {
       return null;
     }
 
-    final textController = StreamController<String>();
-    textController.add(text);
-
     final renderBox = context.findRenderObject() as RenderBox;
     final size = renderBox.size;
 
-    final overlay = OverlayEntry(
-      builder: (context) {
-        return Material(
-          color: Colors.black.withAlpha(150),
-          child: Center(
-            child: Container(
-              constraints: BoxConstraints(
-                maxHeight: size.height * 0.8,
-                minWidth: size.width * 0.5,
-                minHeight: size.height * 0.8,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const SizedBox(height: 10),
-                      const CircularProgressIndicator(),
-                      const SizedBox(height: 10),
-                      StreamBuilder<String>(
-                        stream: textController.stream,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            return Text(snapshot.requireData);
-                          } else {
-                            return Container();
-                          }
-                        },
-                      ),
-                    ],
-                  ),
+    final overLay = OverlayEntry(
+      builder: (context) => Material(
+        color: Colors.black.withAlpha(150),
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(
+              maxHeight: size.height * 0.8,
+              maxWidth: size.height * 0.8,
+              minWidth: size.width * 0.5,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 10),
+                    const CircularProgressIndicator(),
+                    const SizedBox(height: 10),
+                    StreamBuilder(
+                      stream: textController.stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(snapshot.requireData);
+                        } else {
+                          return Container();
+                        }
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
 
-    state.insert(overlay);
-    return LoadingScreenController(
+    state.insert(overLay);
+    LoadingScreenController(
       update: (text) {
         textController.add(text);
         return true;
       },
       close: () {
         textController.close();
-        overlay.remove();
+        overLay.remove();
         return true;
       },
     );
--- lib/Widgets/Loading/loading_screen_controller.dart
+++ lib/Widgets/Loading/loading_screen_controller.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/foundation.dart' show immutable;
 
 typedef UpdateLoadingScreen = bool Function(String text);
-typedef CloseLoadingSCreen = bool Function();
+typedef CloseLoadingScreen = bool Function();
 
 @immutable
 class LoadingScreenController {
   final UpdateLoadingScreen update;
-  final CloseLoadingSCreen close;
+  final CloseLoadingScreen close;
 
   const LoadingScreenController({
     required this.update,
