cd8fb65a426b989b17d9366a3863683dd4ca2c6f
Liman <saniahmadliman@gmail.com>
2024-04-08 11:49:59 -0800

    make some changes

--- lib/Widgets/Loading/loading_screen.dart
+++ lib/Widgets/Loading/loading_screen.dart
@@ -7,17 +7,26 @@ import 'package:instantgram/Widgets/Strings/strings.dart';
 class LoadingScreen {
   LoadingScreen._instance();
   static final shared = LoadingScreen._instance();
-  factory LoadingScreen.instance() => shared;
+  factory LoadingScreen() => shared;
 
   LoadingScreenController? _controller;
 
   void show({
     required BuildContext context,
     String text = Strings.loading,
-  }) {}
+  }) {
+    if (_controller?.update(text) ?? false) {
+      return;
+    } else {
+      _controller = showOverlay(
+        context: context,
+        text: text,
+      );
+    }
+  }
 
   void hide() {
-    _controller?.close();
+    _controller?.close;
     _controller == null;
   }
 
@@ -35,7 +44,64 @@ class LoadingScreen {
     textController.add(text);
 
     final renderBox = context.findRenderObject() as RenderBox;
-    // ignore: unused_local_variable
     final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) {
+        return Material(
+          color: Colors.black.withAlpha(150),
+          child: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxHeight: size.height * 0.8,
+                minWidth: size.width * 0.5,
+                minHeight: size.height * 0.8,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(height: 10),
+                      const CircularProgressIndicator(),
+                      const SizedBox(height: 10),
+                      StreamBuilder<String>(
+                        stream: textController.stream,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return Text(snapshot.requireData);
+                          } else {
+                            return Container();
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    state.insert(overlay);
+    return LoadingScreenController(
+      update: (text) {
+        textController.add(text);
+        return true;
+      },
+      close: () {
+        textController.close();
+        overlay.remove();
+        return true;
+      },
+    );
   }
 }
--- lib/Widgets/Loading/loading_screen_controller.dart
+++ lib/Widgets/Loading/loading_screen_controller.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show immutable;
 
 typedef UpdateLoadingScreen = bool Function(String text);
-typedef CloseLoadingScreen = bool Function();
+typedef CloseLoadingSCreen = bool Function();
 
 @immutable
 class LoadingScreenController {
   final UpdateLoadingScreen update;
-  final CloseLoadingScreen close;
+  final CloseLoadingSCreen close;
 
   const LoadingScreenController({
     required this.update,
