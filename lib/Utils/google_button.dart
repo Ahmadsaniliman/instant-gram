491470b52b825b2dfd31f59c4b7ac5bb1a9f60d4
Liman <saniahmadliman@gmail.com>
2024-04-14 12:49:13 -0800

    make some changes

--- lib/Utils/google_button.dart
+++ lib/Utils/google_button.dart
@@ -1,22 +1,22 @@
-// import 'package:flutter/material.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:instantgram/Widgets/Strings/strings.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instantgram/Widgets/Strings/strings.dart';
 
-// class GoogleButton extends StatelessWidget {
-//   const GoogleButton({super.key});
+class GoogleButton extends StatelessWidget {
+  const GoogleButton({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const SizedBox(
-//       height: 44,
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           FaIcon(FontAwesomeIcons.google),
-//           SizedBox(height: 10),
-//           Text(Strings.google),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 44,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FaIcon(FontAwesomeIcons.google),
+          SizedBox(height: 10),
+          Text(Strings.google),
+        ],
+      ),
+    );
+  }
+}
