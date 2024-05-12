0ac0bd92cc4f71887650cbfc562ce3fe68e288bd
Liman <saniahmadliman@gmail.com>
2024-05-12 12:03:04 -0800

    lets go

--- lib/main.dart
+++ lib/main.dart
@@ -60,11 +60,3 @@ class MyApp extends StatelessWidget {
 }
 
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
