3a06fb7b4bb6b3b7a65703cb0caf3c864461c844
Liman <saniahmadliman@gmail.com>
2024-05-26 11:37:14 -0800

    encouter an error

--- lib/main.dart
+++ lib/main.dart
@@ -57,3 +57,13 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
